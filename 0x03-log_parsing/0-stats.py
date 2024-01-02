90e812a45fc88c2a5301105589802b8bb252f153
Dikachi-official <nwokontadikachi@gmail.com>
2024-01-02 08:14:14 +0100

    utf 8 validation

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,51 +1,3 @@
-#!/usr/bin/python3
-
-<<<<<<< HEAD
-""" script that reads stdin line by line and computes metrics """
-
-import sys
-
-
-def printStatus(dic, size):
-    """ Prints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
-
-
-# sourcery skip: use-contextlib-suppress
-statusCodes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-               "404": 0, "405": 0, "500": 0}
-
-count = 0
-size = 0
-
-try:
-    for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printStatus(statusCodes, size)
-
-        stlist = line.split()
-        count += 1
-
-        try:
-            size += int(stlist[-1])
-        except Exception:
-            pass
-
-        try:
-            if stlist[-2] in statusCodes:
-                statusCodes[stlist[-2]] += 1
-        except Exception:
-            pass
-    printStatus(statusCodes, size)
-
-
-except KeyboardInterrupt:
-    printStatus(statusCodes, size)
-    raise
-=======
 import sys
 
 
@@ -98,4 +50,3 @@ try:
 
 finally:
     print_msg(dict_sc, total_file_size)
->>>>>>> 927c039c3419d8ca8cbe398cbf890d486a60540e
